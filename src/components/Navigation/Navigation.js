375a71d137964ed2e62f39b4ad797ffe66ec00cc
Curtis Hlongwane <curtishlongwane@gmail.com>
2020-07-05 23:50:26 +0200

    App state change works

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-const Navigation = () => {
-	return (
+const Navigation = ({onRouteChange, isSignedIn}) => {
+	if(isSignedIn) {
+		return (
 		<div>
 			<nav className="dt w-100 border-box pa3 ph5-ns">
 			  <div className="dtc v-mid w-75 tr">
-			    <a className="link dim black underline f6 f5-ns dib" href="#" title="Contact">Sign Up</a>
+			    <p onClick = {() => onRouteChange('signout')}className="link dim black underline f6 f5-ns dib">Sign Out</p>
 			  </div>
 			</nav>
 		</div>
 
 		);
+
+	} else{
+		return (
+		<div>
+			<nav className="dt w-100 border-box pa3 ph5-ns">
+			  <div className="dtc v-mid w-100 ph5-l tr">
+			    <p onClick = {() => onRouteChange('signin')}className="link dim black underline f6 f5-ns dib">Sign In</p>
+			  </div>
+			  <div className="dtc v-mid tr">
+			    <p onClick = {() => onRouteChange('register')}className="link dim black underline f6 f5-ns dib">Register</p>
+			  </div>
+			</nav>
+		</div>
+
+		);
+	}
+	
 }
 
 export default Navigation;
